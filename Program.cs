d0b4321bb1345ffccc119db6387d06bd62d5711c
Maria Eduarda Bedetti <100002186+madutiam@users.noreply.github.com>
2022-05-06 19:44:15 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿int numeroDigitado;
+Console.Write("digite um número: ");
+numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+if  (numeroDigitado >= 0)
+{}
+else 
+{
+    Console.WriteLine("Você digitou um número negativo.");
+    }
